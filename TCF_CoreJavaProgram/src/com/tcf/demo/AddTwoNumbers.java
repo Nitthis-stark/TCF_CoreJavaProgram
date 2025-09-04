5924eb3ba0ac20693dea2e9fa6f2ab5d804c0e59
Nitthis <kumarav622@gmail.com>
2025-09-04 11:17:41 +0530

    Day 01 of Java Learning

--- TCF_CoreJavaProgram/src/com/tcf/demo/AddTwoNumbers.java
+++ TCF_CoreJavaProgram/src/com/tcf/demo/AddTwoNumbers.java
@@ -1,8 +1,13 @@
 package com.tcf.demo;
-
+import java.util.*;
 public class AddTwoNumbers {
 
 	public static void main(String[] args) {
+		  Scanner sc=new Scanner(System.in);
+		  int a=sc.nextInt();
+		  int b=sc.nextInt();
+		  System.out.println("The sum of two number is : "+ (a+b));
+		  sc.close();
 	}
 
 }
---
+++ TCF_CoreJavaProgram/src/com/tcf/demo/EvenOdd.java
@@ -0,0 +1,18 @@
+package com.tcf.demo;
+import java.util.*;
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		if((a&1)==1)
+		{
+			System.out.println("The give number is odd");
+		}
+		else {
+			System.out.println("The give number is even");
+		}
+		sc.close();
+	}
+
+}
