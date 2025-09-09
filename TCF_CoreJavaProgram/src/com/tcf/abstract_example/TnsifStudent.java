51ca7e75dcebcd1cab3f102655a8d320830ae62e
Nitthis <kumarav622@gmail.com>
2025-09-09 14:39:52 +0530

    Day 3 of Learning

---
+++ TCF_CoreJavaProgram/src/com/tcf/abstract_example/TnsifStudent.java
@@ -0,0 +1,10 @@
+package com.tcf.abstract_example;
+
+public class TnsifStudent extends Coder{
+	
+	@Override
+	public void practices() {
+		System.out.println("Need to work on TNSIF Codebase at "+wakeuptime);
+	}
+
+}
